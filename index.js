064adae3b4dd30e982a48eee71323e21ad8965c1
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2024-09-02 19:21:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,13 @@
 const app = require('./app'); // Importar la configuración de la aplicación desde app.js
 const http = require('http');
 const cors = require('cors');
+const express = require('express');
+const app = express();
+
+// Configurar middleware
+app.use(express.json());
+app.use(express.static('public')); // Sirve archivos estáticos de la carpeta 'public'
+
 app.use(cors({
   origin: ['https://test-ia-gamma.vercel.app']
 })
