daf0ce0252042ce9560afd0dbc473cb36c143b38
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2024-09-02 19:26:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 // Archivo: index.js
 
-const app = require('./app'); // Importar la configuración de la aplicación desde app.js
 const http = require('http');
-const cors = require('cors');
-const express = require('express');
-const app = express();
+const app = require('./app'); // Importar la configuración de la aplicación desde app.js
 
-// Configurar middleware
-app.use(express.json());
-app.use(express.static('public')); // Sirve archivos estáticos de la carpeta 'public'
+// Iniciar servidor
+const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: ['https://test-ia-gamma.vercel.app']
-})
-);
+const server = http.createServer(app);
 
-
+server.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
